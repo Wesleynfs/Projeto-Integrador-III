d1aa624d46f99f3bcf39754c228ece241577decb
kakoa <kakoa@192.168.1.8>
2020-02-14 00:09:51 -0300

    Adicionando jar postgreSQL

--- Kartodromo/src/kartodromo/Kartodromo.java
+++ Kartodromo/src/kartodromo/Kartodromo.java
@@ -9,9 +9,6 @@ package kartodromo;
  */
 public class Kartodromo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }

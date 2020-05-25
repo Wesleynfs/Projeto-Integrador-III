61519836ddac73571707e653238bfb3bf326f2d6
felipe <felipefillus10@gmail.com>
2020-05-25 10:11:57 -0300

    Mais informações de tutoriais

--- Kartodromo/src/Utilities/InformacoesTutoriais.java
+++ Kartodromo/src/Utilities/InformacoesTutoriais.java
@@ -13,62 +13,36 @@ public class InformacoesTutoriais {
     + "Enviar Avisos aos demais pilotos "
     + "e até mesmo iniciar a corrida</html>",
 
-    "Seu pai",
+    "<html>Todo piloto pode fazer uma avaliação de todos os kartódromos,"
+            + "podendo atribuir uma nota e um comentário"
+            + " para cada um dos kartódromos.</html>",
 
-    "Sua mãe",
+    "<html>Ao tentar fazer uma segunda avaliação sobre um kartódromo,"
+            + " o piloto pode sobrescrever a sua primeira avaliação"
+            + " com a segunda avaliação.</html>",
 
-    "Seu Irmão",
+    "<html>Todo kartódromo pode possuir dias em inatividade,"
+            + " caso o piloto tente criar um campeonato em um desses dias,"
+            + " uma mensagem informando a inatividade será mostrada</html>",
+    
+    "<html>Tipo corrida normal não acrecenta no nível dos pilotos e"
+        + " podem ser iniciadas mesmo com poucos pilotos participando.</html>",
+    
+    "<html>Não é possivél cadastrar nenhum piloto com menos de 12 anos.</html>",
+    
+    "<html>Evite faltar campeonatos para não tomar strikers.</html>",
+    
+    "<html>Campeonatos oficiais podem ser iniciados com ao menos 8 pilotos participando.</html>",
+    
+    "<html>Campeonatos oficiais podem aumentar o nível dos pilotos,"
+            + " quanto melhor for a posição nos campeonatos, maios pontos o piloto conquista. </html>",
+    
+    "<html>Pilotos com menos de 16 anos não podem participar de Tipo corrida Campeonato,"
+            + " toda via podem participar de tipo corrida normal</html>",
+    
+    "<html>Apelidos dos pilotos servem para diferenciar todos os pilotos,"
+            + " para evitar confusões entre os nomes dos pilotos.</html>",
 
-    "Seu cachorro",
-
-    "<html>Ser um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corridaSer um piloto ADM requer aceitar os termos de responsabilidade,"
-    + " mas em compensação o piloto ADM tem permissões de convidar pilotos,"
-    + "Enviar Avisos aos demais pilotos "
-    + "e até mesmo iniciar a corrida</html>"};
+    "Seu cachorro",};
 
 }
